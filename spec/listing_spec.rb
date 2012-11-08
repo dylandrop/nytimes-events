6ecf516ca6d2df243f341694d2666376d7c16c00
Dylan <dylandrop@gmail.com>
2012-11-08 12:45:09 -0500

    added test for successful query

--- spec/listing_spec.rb
+++ spec/listing_spec.rb
@@ -1,9 +1,15 @@
 require 'spec-helper'
 
 describe Nytimes::Events::Listing do
-  it "return nil on 403" do
+  it "raises an exception on 403" do
     listing = Nytimes::Events::Listing.new("XXXX")
     RestClient.stub(:get).and_return(RestClient::Response.create("{}", 403, {}))
     expect {listing.find('foo' => 'bar')}.to raise_error(RestClient::Exception)
+  end
+
+  it "parses the body of the response on 200" do
+    listing = Nytimes::Events::Listing.new("XXXX")
+    RestClient.stub(:get).and_return(RestClient::Response.create("{\"thing1\":\"thing2\"}", 200, {}))
+    listing.find('foo' => 'bar').should == {'thing1' => 'thing2'}
   end
 end
